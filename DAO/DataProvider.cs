9f178d74644262df684ca62d31a71977e77a91ab
OhKa39 <lythanhkhoa360@gmail.com>
2023-12-04 09:09:21 +0700

    Write more script in DataProvider file

--- DAO/DataProvider.cs
+++ DAO/DataProvider.cs
@@ -6,6 +6,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Data.Common;
 
 namespace DAO
 {
@@ -30,7 +32,64 @@ namespace DAO
         public DataProvider() { }
         public DataTable ExcuteQuery(string query, object[] parameter = null)
         {
-            using( sqlco)
+            using(SqlCommand command = new SqlCommand(conn))
+            {
+                string[] stringQuery = query.Split(' ');
+                int j = 0;
+                foreach(var i in stringQuery)
+                {
+                    if(i.Contains('@'))
+                    {
+                        command.Parameters.AddWithValue(i, parameter[j]);
+                        ++j;
+                    }    
+                }
+
+                SqlDataAdapter da = new SqlDataAdapter(command);
+                DataTable data = new DataTable();
+
+                da.Fill(data);
+                return data;
+            }
+        }
+
+        public int ExcuteNonQuery(string query, object[] parameter = null, string action = null)
+        {
+            int count = 0;
+
+            using (SqlCommand command = new SqlCommand(conn))
+            {
+                string[] stringQuery = query.Split(' ');
+
+                int j = 0;
+                foreach (var i in stringQuery)
+                {
+                    if (i.Contains('@'))
+                    {
+                        command.Parameters.AddWithValue(i, parameter[j]);
+                        ++j;
+                    }
+                }
+
+                SqlDataAdapter da = new SqlDataAdapter();
+                switch (action)
+                {
+                    case "Insert":
+                        da.InsertCommand = command;
+                        break;
+                    case "Delete":
+                        da.DeleteCommand = command;
+                        break;
+                    case "Update":
+                        da.UpdateCommand = command;
+                        break;
+                }
+
+                DataTable data = new DataTable();
+
+                return count;
+            }
+
         }
     }
 }
